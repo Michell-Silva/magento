8a2dde7da88024c8a66dff9651395f2bb9b81b0c
Michell Silva <michell.silva@marabraz.com>
2019-03-16 21:54:33 -0300

    primeiro commit

---
+++ test.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>TESTE DO BOTÃO</title>
+</head>
+<body>
+<h1>teste de input</h1>
+<form action="cortina.php" method="POST">
+
+NOME:       <input type="text" name=" nome" ><br><br>
+EMAIL:      <input type="email" name=" email"><br>
+<input type="submit" name="envia" value="Envia" >
+
+</form>
+
+</body>
+</html>
